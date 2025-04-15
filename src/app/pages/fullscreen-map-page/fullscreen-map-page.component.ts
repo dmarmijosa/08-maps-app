11168e86a9a897effadf75731b76aba3bf85f103
Danny Armijos <daniel2667@hotmail.es>
2025-04-15 02:22:51 +0200

    feat: integrate Mapbox GL JS and update fullscreen map component

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { environment } from '@environments/environment';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
+mapboxgl.accessToken = environment.mapBoxKey;
 @Component({
   selector: 'app-fullscreen-map-page',
   imports: [],
   templateUrl: './fullscreen-map-page.component.html',
+  styles: `
+    div{
+      width:100vw;
+      height: calc(100vh - 64px);
+    }
+
+  `,
 })
-export class FullscreenMapPageComponent { }
+export class FullscreenMapPageComponent implements AfterViewInit {
+  divElemnt = viewChild<ElementRef>('map');
+
+  async ngAfterViewInit() {
+    await new Promise((resolve) => setTimeout( resolve, 80));
+    if (!this.divElemnt()?.nativeElement) return;
+    const element = this.divElemnt()?.nativeElement;
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+}
