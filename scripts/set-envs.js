4270542c725ce5af23a35134f01c436ca1e32db5
Danny Armijos <daniel2667@hotmail.es>
2025-04-15 02:03:27 +0200

    feat: add environment configuration and MapBox API integration

---
+++ scripts/set-envs.js
@@ -0,0 +1,22 @@
+const { writeFileSync, mkdirSync } = require("fs");
+
+require("dotenv").config();
+
+const targetPath = "./src/environments/environment.ts";
+const targetPathDev = "./src/environments/environment.development.ts";
+
+const mapBoxKey = process.env["MAPBOX_KEY"];
+
+if (!mapBoxKey) {
+  throw new Error("MAPBOX_KEY is not set");
+}
+
+const envFileContent = `
+export const environment = {
+  mapBoxKey: '${mapBoxKey}',
+};
+`;
+
+mkdirSync('./src/environments', {recursive: true});
+writeFileSync(targetPath, envFileContent);
+writeFileSync(targetPathDev, envFileContent);
